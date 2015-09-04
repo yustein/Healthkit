c252f0eaeeb980ec987cb7981ac9fcb2895a40bc
Yo Wakita <wakita@gmail.com>
2015-09-03 23:27:28 -0700

    Adds total step count of last 24 hours. Also adds sprite images for testing of animation. Also adds jQuery. To be honest I should have commited more along the way.

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,3 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform, $cordovaHealthKit) {
@@ -41,28 +35,28 @@ angular.module('starter', ['ionic', 'ngCordova'])
 
 .controller('AppCtrl', function($scope, $cordovaHealthKit) {
 
-  var stepsCount = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
-  var sumOption = HKStatisticsOptions.CumulativeSum
-    $scope.getStepCount = function() {
-      $cordovaHealthKit.querySampleType(
-          {
-              "startDate" : new Date(new Date().getTime() - 2*24*60*60*1000),
-              "endDate"   : new Date(),
-              "sampleType": "HKQuantityTypeIdentifierStepCount",
-              "unit"      : "count"
-          }
-          
-      ).then(function(v) {
-          alert(JSON.stringify(v));
-      }, function(err) {
-          console.log(err);
-      });
-    };
- 
-    $scope.getTotalSteps = function() {
-      $cordovaHealthKit.HKStatisticsQuery(quantityType: stepsCount, quantitySamplePredicate: nil,
-    options: sumOption)
+  // var stepsCount = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount)
+  // var sumOption = HKStatisticsOptions.CumulativeSum
+  $scope.getStepCount = function() {
+    $cordovaHealthKit.querySampleType(
+        {
+            "startDate" : new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
+            "endDate"   : new Date(),
+            "sampleType": "HKQuantityTypeIdentifierStepCount",
+            "unit"      : "count"
+        }
+        
+    ).then(function(v) {
+      var cumulativeSum = 0
+      for (var i = 0; i < v.length; i++) {
+        cumulativeSum = cumulativeSum + v[i]["quantity"]
+      };  
+      alert("You walked " + cumulativeSum + " steps in the last 24 hours."); 
 
 
-    }
+    }, function(err) {
+        console.log(err);
+    });
+  };
 });
+
